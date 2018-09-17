36e86cc917cf62f664731c1e6c4a42dd318e7c74
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-09-17 13:37:07 -0400

    Add files via upload

---
+++ Calculator Example/src/CalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class CalculatorMain 
+{
+
+	public static void main(String[] args) 
+	{
+		// This is how we create an object
+		// new --> java keyword which creates an object
+		Calculator calc = new Calculator(); 
+		System.out.print(calc.add(2, 6));
+	}
+}
